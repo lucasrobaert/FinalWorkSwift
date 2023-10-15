85393bbc3feeefe563f3d616178303099b915a0d
Lucas Robaert <lucasrobaert@hotmail.com>
2023-10-15 16:36:41 -0300

    Initial Commit

---
+++ FinalWorkSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalWorkSwift
+//
+//  Created by Lucas Robaert on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinalWorkSwift/FinalWorkSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalWorkSwiftApp.swift
+//  FinalWorkSwift
+//
+//  Created by Lucas Robaert on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalWorkSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
